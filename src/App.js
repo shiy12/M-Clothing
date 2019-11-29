d7cd3e07e16f6916bebecc7eac5530b78c9cb7b2
shiy12 <809297016@qq.com>
2019-11-29 18:23:13 -0500

    provide navigation using withRouter() in react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import {Switch,Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.components';
 
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component = {HomePage}/>
+        <Route path='/shop/hats' component = {HatsPage}/>
+      </Switch>
     </div>
   );
 }
